1958e71a34fd2f832a9423146d87af5316273768
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2022-03-16 22:43:48 +0000

    feat: add conditions for win/lose

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,70 @@
 <script lang="ts">
-	const boardSizeX = 5;
-	const boardSizeY = 5;
+	
+	let previousGuesses = [];
+	let guess = "";
+	const mapping = {};
+	const fakeWord = "world";
+	const boardSizeX = fakeWord.length;
+
+	let currentGuessAttempt = 0;
+	const totalGuessAttempt = 6;
+
+	function onSubmit(e) {
+		e.preventDefault();
+		console.log(guess);
+		
+		if (guess === fakeWord) {
+			alert("you are a winner")
+			// TODO: fire off some confetti
+			return
+		}
+
+		previousGuesses.push(guess);
+		currentGuessAttempt++;
+		if (currentGuessAttempt === totalGuessAttempt) {
+			alert("you lost!")
+			return
+		}
+		
+		const letters = guess.split("")
+		
+		letters.forEach((letter, i) => {
+			// yellow if letter is present in word
+			const isPresent = fakeWord.includes(letter);
+			// green if letter is present AND in correct position
+			if (isPresent) {
+				const isCorrectPosition = fakeWord.charAt(i) === letter
+				
+				if (isCorrectPosition) {
+					mapping[letter] = "green"
+					return
+				}
+				mapping[letter] = "yellow"
+			}
+			mapping[letter] = "grey"
+		});
+		
+	}
 
 </script>
 
 <main>
-	<div class="grid">
-		{#each Array(boardSizeY) as _, row (row)}
-			{#each Array(boardSizeX) as _, col (col)}
-				<div class="grid-item">{row}{col}</div>
+	<form on:submit={onSubmit}>
+		<p>guesses remaining: {totalGuessAttempt - currentGuessAttempt}</p>
+		<input type="text" bind:value={guess} />
+		<div class="grid">
+			{#each Array(totalGuessAttempt) as _, row (row)}
+				{#each Array(boardSizeX) as _, col (col)}
+				<!-- TODO: fill the characters on the board -->
+					{#if row === currentGuessAttempt && col < guess.length && guess}
+						<div class="grid-item">{guess[col]}</div>
+					{:else}
+						<div class="grid-item"></div>
+					{/if}
+				{/each}
 			{/each}
-		{/each}
-	</div>
+		</div>
+	</form>
 </main>
 
 <style>
@@ -34,5 +87,6 @@
 		text-align: center;
 		justify-self: center;
 		padding: 20px;
+		text-transform: uppercase;
 	}
 </style>
